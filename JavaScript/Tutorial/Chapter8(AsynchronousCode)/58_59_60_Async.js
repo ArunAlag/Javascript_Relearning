d1cdb56b8da27b9cf09a37aaf75fabd118873f58
Arun_Alag <arunalag3003@gmail.com>
2024-08-16 20:28:33 +0800

    Started with chapter 8, Asynchronous Code

---
+++ JavaScript/Tutorial/Chapter8(AsynchronousCode)/58_59_60_Async.js
@@ -0,0 +1,5 @@
+// definition : aysnchronous code are codes that run indepedentantly of the main program flow. It allows program to run other task while waiting for it's operation to complete
+// => Mainly used when operations take unpredictable time. It's used mostly in network request, like client requesting data from server and etc.
+// => There are 3 types of asynchronous way of writing code which will be discussed below
+
+// => Method 1: regular callback code
