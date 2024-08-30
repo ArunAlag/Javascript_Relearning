b1917dbc3e0ec65daa2fc010d3ad769e0840568f
Arun_Alag <arunalag3003@gmail.com>
2024-08-31 00:28:29 +0800

    Continue with microproject 10, Now I can load the days relative to the current date and manage disabled the days that are not part of the month

--- JavaScript/Tutorial/Micro_Projects/Date_Picker(Chap10)/index.js
+++ JavaScript/Tutorial/Micro_Projects/Date_Picker(Chap10)/index.js
@@ -1,9 +1,10 @@
 // * Required imports
-import { format, eachDayOfInterval, startOfMonth, endOfMonth } from "date-fns";
+import { format, eachDayOfInterval, startOfMonth, endOfMonth, getDay, subDays, addDays } from "date-fns";
 
 // * DOM Elements
 const date_button = document.querySelector(".date-picker-button");
 const date_picker = document.querySelector(".date-picker");
+const date_picker_grid = document.querySelector(".date-picker-grid");
 
 init();
 
@@ -20,12 +21,51 @@ function init() {
   date_button.textContent = formattedDate;
 }
 
-function loadCurrentMonth(date) {
+function loadCurrentMonth(dateText) {
+  const date = new Date(dateText);
+  const calendarDays = generateDatesRange(date);
+
   const days = eachDayOfInterval({
-    start: startOfMonth(date),
-    end: endOfMonth(date),
+    start: calendarDays.start_range,
+    end: calendarDays.end_range,
   });
 
-  const dayNumbers = days.map((day) => day.getDate());
-  console.log(dayNumbers);
+  // => Convert the days into dates
+  const currentMonth = date.getMonth();
+
+  days.forEach((day) => {
+    const dayMonth = day.getMonth();
+    const button = document.createElement("button");
+    button.textContent = day.getDate();
+    button.classList.add("date");
+
+    if (dayMonth !== currentMonth) {
+      button.classList.add("date-picker-other-month-date");
+    }
+
+    date_picker_grid.append(button);
+  });
+}
+
+function generateDatesRange(date) {
+  // => Get the start day of the month
+  const startDate = startOfMonth(date);
+
+  // => Get the end day of the month
+  const endDate = endOfMonth(date);
+
+  // => Get the first day alignment
+  const firstDayAlignment = getDay(startDate);
+
+  // => Calculate how many days must be added before the actual date
+  const previousMonthDates = subDays(startDate, firstDayAlignment);
+
+  // => Get the last day alignment
+  const lastDayAlignment = getDay(endDate);
+
+  // => Calculate how many days must be added after the end date of the month
+  const days_to_add = 6 - lastDayAlignment;
+  const nextMonthDates = addDays(endDate, days_to_add);
+
+  return { start_range: previousMonthDates, end_range: nextMonthDates };
 }
